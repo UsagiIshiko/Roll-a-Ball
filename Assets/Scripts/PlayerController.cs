ff9dd36aba3a088fb35c66006dbddc49264ef424
Uasgi Ishiko <usagisoftware@gmail.com>
2017-01-18 14:10:57 +0900

    3. Collecting, Scoring and Building the Game - 03. Displaying the Score and Text
    -Modified PlayerController Script to add a score everytime pick up GameObject was disabled.
    -Added UI Element Count Text
    -Added UI Element Win Text
    -Modified PlayerController Script to display count information in Count Text UI Element
    -Modified PlayerController Script to display win inforkation in Win Text when win codntion is reached. (All 12 collectables are collected by player GameObject)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,40 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Needed for UI Element Code
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
-    public Rigidbody rb;
+    private Rigidbody rb;
+    private int count;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     // Physics Check Update
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
     }
-    
+
     // When Player GameObject hits the collider of Pick Up GameObject, disable the GameObject.
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
